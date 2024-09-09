d5cdbe50fd40810a33516617381b7a4684249d3a
mubashir4418 <103724630+mubashir4418@users.noreply.github.com>
2024-09-09 09:52:20 +0500

    Initial commit

---
+++ flutter_practice_app/lib/wrap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Wrap extends StatefulWidget {
+  const Wrap({super.key});
+
+  @override
+  State<Wrap> createState() => _WrapState();
+}
+
+class _WrapState extends State<Wrap> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
